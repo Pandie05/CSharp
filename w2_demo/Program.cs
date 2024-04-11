a208bfd00956ea8dddbe4fd4daac7a216012a8d1
Pandie05 <hreyes@email.neit.edu>
2024-04-11 09:47:55 -0400

    demo done

--- w2_demo/Program.cs
+++ w2_demo/Program.cs
@@ -6,9 +6,25 @@ using System.Threading.Tasks;
 
 namespace w2_demo {
     class Program {
+
+        static string reverseString(string str) {
+
+            string reversed = "";
+            
+            Console.WriteLine("input: " + str);
+
+            for (int i = str.Length - 1; i >= 0; i--) {
+
+                reversed += str[i];
+            }
+
+            return reversed;
+
+        }
+
         static void Main (string[] args) { 
 
-            for (int i = 1; i <= 10; i++) {
+           /*  for (int i = 1; i <= 10; i++) {
 
                 for (int j = 1; j <= 10; j++) {
 
@@ -17,9 +33,13 @@ namespace w2_demo {
                 }
 
                 Console.WriteLine ($"\n");
-               
+                
             }
+            */
+
+            Console.WriteLine($"Output: {reverseString("TacocaT | RacecaR | HannaH")}");
 
         }
+
     }
 }        
