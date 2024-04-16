da6119b392ab0b0ce23d4bd4e37b71d9ea1d40c6
Pandie05 <hreyes@email.neit.edu>
2024-04-16 09:22:01 -0400

    doodoo

--- w3-assignment/Program.cs
+++ w3-assignment/Program.cs
@@ -8,10 +8,36 @@ namespace w3_assignment
 {
     class Program
     {
+
+        static void GetRandomPoint(ref Point p)
+        {
+            Random r = new Random();
+            p.X = r.Next(0, 100);
+            p.Y = r.Next(0, 100);
+        }
+
         static void Main(string[] args)
         {
         
-            
+            //define point structure
+            struct Point
+            {
+                public int X;
+                public int Y;
+
+                public Point(int x, int y)
+                {
+
+                    X = x;
+                    Y = y;
+
+                }
+
+            }
+
+            //create a point object
+
+            Point p1 = new Point(10, 20);
 
         }
     }
