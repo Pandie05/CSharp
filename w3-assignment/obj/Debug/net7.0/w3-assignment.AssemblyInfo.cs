7d6296c42a6455766b56c89f5aa4b45dfae2c8b3
Pandie05 <hreyes@email.neit.edu>
2024-05-08 15:14:05 -0400

    dook

--- w3-assignment/obj/Debug/net7.0/w3-assignment.AssemblyInfo.cs
+++ w3-assignment/obj/Debug/net7.0/w3-assignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("w3-assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77dab6687827d299d08602ae771405c563886ce4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e09069eaba71d1dc5d1d8e28149d626c49da16")]
 [assembly: System.Reflection.AssemblyProductAttribute("w3-assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("w3-assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
